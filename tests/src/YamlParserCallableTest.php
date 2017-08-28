6e771a96d577e5bec95cf74e7742bca58abb116d
Carsten Witt <carsten.witt@gmail.com>
2017-08-28 14:39:56 +0200

    Added test for YamlParserCallable

---
+++ tests/src/YamlParserCallableTest.php
@@ -0,0 +1,34 @@
+<?php
+namespace tests;
+
+use Germania\YamlServices\YamlParserCallable;
+use Pimple\Container;
+use Symfony\Component\Finder\Finder;
+use Psr\Log\NullLogger;
+use Psr\Log\LoggerInterface;
+
+class YamlParserCallableTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @dataProvider provideCtorArguments
+     */
+    public function testInstantiation( $flags, $logger )
+    {
+        $sut = new YamlParserCallable( $flags, $logger );
+
+    }
+
+
+
+    public function provideCtorArguments()
+    {
+        $logger = $this->prophesize(LoggerInterface::class);
+        $logger_mock = $logger->reveal();
+
+        return [
+            [ 0, $logger_mock ]
+        ];
+    }
+}
+
